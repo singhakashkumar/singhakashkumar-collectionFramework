84aa6181adfd56570e623ec42d439dc9e8aeb11e
singhakashkumar <singhakashkumar.a@gmail.com>
2020-03-06 22:19:10 +0530

    Collection Framework

---
+++ src/test/java/com/epam/CollectionFrameWork/AppTest.java
@@ -0,0 +1,20 @@
+package com.epam.CollectionFrameWork;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit testing.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Testing ;/
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
